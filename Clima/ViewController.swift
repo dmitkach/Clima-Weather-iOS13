e131ddc010382de07839f0fe64e714a577ecb246
Dmitry Tkach <dmitkach@icloud.com>
2020-06-19 15:46:53 +0300

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Dmitry Tkach on 19.06.2020.
+//  Copyright © 2020 Dmitry Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
